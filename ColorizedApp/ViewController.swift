ac197161f53225fa4004cf0940b9009ab140f96b
lsvard <l9244492@bk.ru>
2023-08-11 17:48:33 +0300

    Work with logic

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -21,25 +21,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 15
+        
         setupSlider()
         redSliderAction()
         greenSliderAction()
         blueSliderAction()
-        
     }
     
     // MARK: - IBActions
     @IBAction func redSliderAction() {
-        //redLabel.text = String(redSlider.value)
-        redLabel.text = "Red: \(redSlider.value.formatted())"
+        redLabel.text = String(format: "Red: %.2f", redSlider.value)
+        changeViewColor()
     }
     
     @IBAction func greenSliderAction() {
-        
+        greenLabel.text = String(format: "Green: %.2F", greenSlider.value)
+        changeViewColor()
     }
     
     @IBAction func blueSliderAction() {
-        
+        blueLabel.text = String(format: "Blue: %.2F", blueSlider.value)
+        changeViewColor()
     }
     
     // MARK: - Private Methods
@@ -47,6 +49,15 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    private func changeViewColor() {
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
 }
 
