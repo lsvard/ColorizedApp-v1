f0a8bf317e7d65ae1ff4199c84b51df35df8a657
lsvard <l9244492@bk.ru>
2023-08-11 17:55:58 +0300

    Optimisation logic

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -21,25 +21,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 15
-        
         setupSlider()
-        redSliderAction()
-        greenSliderAction()
-        blueSliderAction()
+        sliderAction()
     }
     
     // MARK: - IBActions
-    @IBAction func redSliderAction() {
+    @IBAction func sliderAction() {
         redLabel.text = String(format: "Red: %.2f", redSlider.value)
-        changeViewColor()
-    }
-    
-    @IBAction func greenSliderAction() {
         greenLabel.text = String(format: "Green: %.2F", greenSlider.value)
-        changeViewColor()
-    }
-    
-    @IBAction func blueSliderAction() {
         blueLabel.text = String(format: "Blue: %.2F", blueSlider.value)
         changeViewColor()
     }
